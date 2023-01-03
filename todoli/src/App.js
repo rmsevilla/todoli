ffbf12a941465686d67f8e77127116833fedcb7c
rmsevilla <70169981+rmsevilla@users.noreply.github.com>
2023-01-03 15:12:20 -0800

    Current progress of ListContentDisplay

--- todoli/src/App.js
+++ todoli/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateList from './component/CreateList/CreateList';
+import ListContent from './component/ListContent/ListContent';
 
 
 function App() {
   return (
-    <CreateList></CreateList>
+    // <CreateList></CreateList>
+    <ListContent></ListContent>
   );
 }
 
--- todoli/src/component/ListContent/ListContent.js
+++ todoli/src/component/ListContent/ListContent.js
@@ -1,10 +1,12 @@
 import ListContentFooter from "./ListContentFooter";
 import ListContentHeader from "./ListContentHeader";
+import ListContentDisplay from "./ListContentDisplay";
 
 function ListContent(){
     return(
         <><div>
             <ListContentHeader></ListContentHeader>
+            <ListContentDisplay></ListContentDisplay>
         </div><ListContentFooter></ListContentFooter></>
     )
 }
