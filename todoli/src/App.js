b30717000a00e6bc6795e8d80a4f9fd68a3dc360
Teezey <yibiaojob@gmail.com>
2023-01-03 13:43:33 -0800

    stuff

--- todoli/src/App.js
+++ todoli/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import CreateList from './component/CreateList/CreateList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CreateList from './component/CreateList/CreateList';
 
 
 function App() {
