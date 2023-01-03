cfc605325a7f2ac920be62c3081bd87c57517572
Teezey <yibiaojob@gmail.com>
2023-01-03 15:49:02 -0800

    add routing and link

--- todoli/src/App.js
+++ todoli/src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateList from './component/CreateList/CreateList';
 import ListContent from './component/ListContent/ListContent';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    // <CreateList></CreateList>
-    <ListContent></ListContent>
+     //<CreateList></CreateList>
+    //<ListContent></ListContent>
+    <BrowserRouter>
+    <Routes>
+        <Route index element={<CreateList />} />
+        <Route path="listcontent" element={<ListContent />} />
+        {/* <Route path='listcontent/grocery' element={<ListContent prop="grocery" ></ListContent>} */}
+    </Routes>
+  </BrowserRouter>
   );
 }
 
